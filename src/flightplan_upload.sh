9e98032538d20bfa9497b8acc4959cd1f7645c16
RMalone8 <rmalone@bu.edu>
2026-02-26 12:35:42 -0500

    env file changes

--- src/flightplan_upload.sh
+++ src/flightplan_upload.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-DRONE_PI_IP="10.10.10.10" # will be configured
-DRONE_PI_USER="sr-design"
-LOCAL_FILE="/home/sr-design/agrodrone-system/flightplans/waypoints.json"
-REMOTE_DEST="/home/sr-design/agrodrone-system/flightplans/"
-
 rsync -az $LOCAL_FILE $DRONE_PI_USER@$DRONE_PI_IP:$REMOTE_DEST
