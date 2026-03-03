23407a2195b33a8387e3070ed746d7e5679b2503
RMalone8 <rmalone@bu.edu>
2026-03-03 15:37:56 -0500

    telemetry triggered by change

--- src/telemetry.py
+++ src/telemetry.py
@@ -2,7 +2,6 @@ import json
 from dotenv import load_dotenv
 import os
 import requests
-import time
 
 load_dotenv()
 
@@ -10,16 +9,23 @@ BACKEND_URL = os.getenv("BACKEND_URL")
 DEVICE_TOKEN = os.getenv("DEVICE_TOKEN")
 TELEMETRY_PATH = os.getenv("TELEMETRY_PATH")
 
-while True:
+def main():
     # read the telemetry file
-    with open(TELEMETRY_PATH, "r") as f:
-        telemetry = json.load(f)
-
-    print(telemetry)
+    try:
+        with open(TELEMETRY_PATH, "r") as f:
+            telemetry = json.load(f)
+    except Exception as e:
+        print(f"Error Opening Telemetry File at {TELEMETRY_PATH}: ", e)
 
     # send telemetry
-    response = requests.post(BACKEND_URL + "/telemetry",
-                headers={"Authorization": "Bearer " + DEVICE_TOKEN},
-                json=telemetry)
+    try:
+        response = requests.post(BACKEND_URL + "/telemetry",
+                    headers={"Authorization": "Bearer " + DEVICE_TOKEN},
+                    json=telemetry)
+    except Exception as e:
+        print("Error Sending Telemetry to Backend: ", e)
 
-    time.sleep(15)
+    print(response, response.status_code)
+
+if __name__ == '__main__':
+    main()
